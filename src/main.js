619587900bfa9d3c5456772453ba9596be51b104
jangheonyeong <wkdgjsdud@snu.ac.kr>
2025-06-23 11:39:03 +0900

    sweetalert2 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+import Swal from 'sweetalert2';
+
 const button = document.getElementById("warning-button");
 
 button.addEventListener("click", () => {
-  alert("클릭하지 마세요");
+  Swal.fire({
+    icon: 'warning',
+    title: '경고',
+    text: '클릭하지 마세요!',
+    confirmButtonText: '알겠습니다',
+    confirmButtonColor: '#66bb6a', // 파스텔 초록
+    background: '#e8f5e9',
+    color: '#2e7d32'
+  });
 });
